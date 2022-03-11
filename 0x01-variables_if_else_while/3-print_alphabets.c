140b3a10e65ab2112c454c92d8d892d700ff9894
Alvin-Ochieng <ochiengalvin465@gmail.com>
2022-03-11 19:28:20 +0300

    fix syntax error

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	char ch;
+	int ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
 	for (ch = 'A'; ch <= 'Z'; ch++)
 		putchar(ch);
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
