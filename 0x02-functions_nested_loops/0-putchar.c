2b0d00295a71f453996391d894b9fb1c0568dfb8
Alvin-Ochieng <ochiengalvin465@gmail.com>
2022-03-15 22:34:31 +0300

    Fix syntax error

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * main - prints _putchar, followed by a new line
  * Return: Always 0 (Success)
@@ -5,15 +6,15 @@
 
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
