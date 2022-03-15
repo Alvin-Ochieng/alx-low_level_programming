877d69ad9a5dec35e2e2d787ae441319c6bc0ad2
Alvin-Ochieng <ochiengalvin465@gmail.com>
2022-03-16 02:13:22 +0300

    fix betty coding style error

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -6,7 +6,7 @@
  * @b: int to be added to a
  * Return: sum of a and b
  */
-int add(a, b)
+int add(int a, int b)
 {
 	int c;
 
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
  /**
-  * @print_sign - prints the sign of a number
+  * print_sign(int n) - prints the sign of a number
   * @n: the int to check
   * Return: 1 and prints + if n is greater than zero
   * 0 and prints 0 if n is zero
