43b8e4785db65be4a0b3a3f1417de11b318f3bec
Alvin-Ochieng <ochiengalvin465@gmail.com>
2022-03-16 02:48:12 +0300

    fix betty coding style error

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,12 +1,12 @@
 #include "main.h"
  /**
-  * print_sign(int n) - prints the sign of a number
+  * print_sign - prints the sign of a number
   * @n: the int to check
+  *
   * Return: 1 and prints + if n is greater than zero
   * 0 and prints 0 if n is zero
   * -1 and prints - if n is less than zero
   */
-/*print_sign - prints the sign of a number*/
 int print_sign(int n)
 {
 	if (n > 0)
