3b4501e8db5abaddc98fc3c6c63662c8a1dab80f
Alvin-Ochieng <ochiengalvin465@gmail.com>
2022-03-16 01:18:00 +0300

    Add files 2 through 16 to project 0x02 C

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @int: int to be added to int
+ * @int: int to be added to int
+ * Return: sum of int and int
+ */
+int add(int, int)
+{
+	int c;
+
+	c = int + int;
+
+	return (c);
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ * Return: 1 if c is lowercase, o otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
